c6f78090e848a50d3626c3490a1029f1de24d4bd
uskay <uskay@google.com>
2021-07-29 01:33:13 +0900

    consider proxying all the web resources e.g. images

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,18 @@
 const functions = require("firebase-functions");
 const fetch = require('node-fetch');
 
-
 exports.proxy = functions.https.onRequest(async (request, response) => {
+  const imgExtension = true; // request.url.match(/\.(png|jpg|jpeg|gif|svg)/);
   const fetchURL = `http://morinooto.jp${request.url}`;
   const res = await fetch(fetchURL);
-  const html = await res.text();
-  response.send(html);
+  const blob = await res.blob();
+  response.type(blob.type);
+  if(blob.type.includes('text/html')) {
+    let html = await blob.text();
+    html = html.replace(/morinooto\.jp/g, 'morinoototest.net');
+    response.send(html);
+    return;
+  }
+  const buffer = await blob.arrayBuffer();
+  response.send(Buffer.from(buffer));
 });
