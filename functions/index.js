db2bd02b8081d33f924798c09d2f06888d718db0
uskay <uskay@google.com>
2021-07-29 01:33:36 +0900

    refactoring

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require("firebase-functions");
 const fetch = require('node-fetch');
 
 exports.proxy = functions.https.onRequest(async (request, response) => {
-  const imgExtension = true; // request.url.match(/\.(png|jpg|jpeg|gif|svg)/);
   const fetchURL = `http://morinooto.jp${request.url}`;
   const res = await fetch(fetchURL);
   const blob = await res.blob();
